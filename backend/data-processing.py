250711229c1d35a47d5bf92858abaf477dddae8e
timuriz <timur.izmailov.lv@gmail.com>
2026-03-26 10:35:02 +0100

    +load_data +normalize the columns +clen the data

--- backend/data-processing.py
+++ backend/data-processing.py
@@ -1,11 +1,28 @@
 import pandas as pd
 
-
-
 def load_data(path):
     df = pd.read_csv(path)
     return df
 
 if __name__ == "__main__":
   df = load_data("/Users/timur/Downloads/Expenses_clean.csv")
-  print(df.head())
+  #print(df.head())
+
+df_standarized = df["date_time"]
+
+def normalize_columns(df):
+   df = df.rename(columns={"date_time": "date", "category": "description"})
+   return df
+
+df = normalize_columns(df)
+
+def clean_data(df):
+   df["date"] = pd.to_datetime(df["date"])
+   df["amount"] = df["amount"].astype(float)
+   df["type"] = df["amount"].apply(lambda x: "debit" if x < 0 else "credit")
+
+   return df
+
+df = clean_data(df)
+
+print(df.head())
