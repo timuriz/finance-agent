ba30bfe56b1589ace64731b2318f313cdf42f099
timuriz <timur.izmailov.lv@gmail.com>
2026-04-04 21:49:49 +0200

    Refactor analytics functions to improve spending calculations and add overspending detection

--- backend/analytics.py
+++ backend/analytics.py
@@ -2,7 +2,17 @@ def total_spent(df):
     return df["amount"].sum()
 
 def spending_by_category(df):
-    return df.groupby("category")["amount"].sum()
+    return df.groupby("category")["amount"].sum().sort_values()
 
 def top_category(df):
     return df.groupby("category")["amount"].sum().idxmin()
+
+def category_by_percentage(df):
+    total = abs(df["amount"].sum())
+
+    return (abs(spending_by_category(df)) / total ) * 100
+
+def detect_overspending(df):
+    percentages = category_by_percentage(df)
+
+    return percentages[percentages > 30]
